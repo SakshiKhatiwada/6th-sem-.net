4ce66d3e82d0aeb7bbfae5b07235e2cd2074f324
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-07-10 06:48:48 +0545

    first project

---
+++ Project-1-The-First-Project/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Project1
+{
+
+       class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Sakshi!");
+            // var x = Console.ReadLine();
+            // Console.WriteLine("hi " + x);
+            // Console.WriteLine(Console.ReadKey());
+            ConsoleKeyInfo clickedKey = Console.ReadKey();
+            Console.Write("\n");
+            Console.WriteLine(clickedKey.Key);
+            Console.WriteLine(clickedKey.KeyChar);
+            Console.WriteLine(clickedKey.Modifiers);
+        }
+    }
+}
