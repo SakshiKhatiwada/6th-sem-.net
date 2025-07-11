4b7284055005f7a672bc5670eb095857f958c148
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-07-11 07:15:28 +0545

    variables in classrom

--- Project-1-The-First-Project/Program.cs
+++ Project-1-The-First-Project/Program.cs
@@ -8,15 +8,30 @@ namespace Project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Sakshi!");
+            // SECTION Day 1
+            // Console.WriteLine("Hello, Sakshi!");
             // var x = Console.ReadLine();
             // Console.WriteLine("hi " + x);
             // Console.WriteLine(Console.ReadKey());
-            ConsoleKeyInfo clickedKey = Console.ReadKey();
-            Console.Write("\n");
-            Console.WriteLine(clickedKey.Key);
-            Console.WriteLine(clickedKey.KeyChar);
-            Console.WriteLine(clickedKey.Modifiers);
+            // ConsoleKeyInfo clickedKey = Console.ReadKey();
+            // Console.Write("\n");
+            // Console.WriteLine(clickedKey.Key);
+            // Console.WriteLine(clickedKey.KeyChar);
+            // Console.WriteLine(clickedKey.Modifiers);
+
+            // SECTION Day 2
+            // C# Variables example
+            string name = "Ram Bahadur";
+            int age = 22;
+            // C# variables with concatenation example.
+            Console.WriteLine("Hello everyone! My name is "+name+" and my age is "+age);
+
+            Console.WriteLine("Enter Your Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello everyone! My name is "+name+" and my age is "+age);
+
         }
     }
 }
