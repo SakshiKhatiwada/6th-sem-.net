37f441bb00d1ab71ef58265766d83d425b57b23b
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-07-11 07:43:07 +0545

    class and object concept

---
+++ Project-2_Class-and-Object/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Teacher
+{
+
+class Teacher{
+// Field = Class-level variable that holds the value
+private int id;
+
+// Property = encapsulate a private field to assign underlying field value
+public int TeacherId{
+    get{return id;}
+    set{
+        if(value>0)
+        id = value;}
+}
+
+public Teacher(){
+    Console.WriteLine("This is constructor");
+}
+static void Main(string[] args)
+{
+    // Objects of the class
+
+Teacher T1 = new Teacher();
+T1.TeacherId = 1;
+Console.WriteLine(T1.TeacherId);
+}
+
+
+}
+}
