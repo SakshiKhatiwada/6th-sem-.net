f1ec0dce41d7ab3dccde18e35593f99f12d8b851
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-07-13 06:55:52 +0545

    arithmetic operator, even check, incomplete: tried pattern making

--- Project-1-The-First-Project/Program.cs
+++ Project-1-The-First-Project/Program.cs
@@ -21,16 +21,59 @@ namespace Project1
 
             // SECTION Day 2
             // C# Variables example
-            string name = "Ram Bahadur";
-            int age = 22;
+            // string name = "Ram Bahadur";
+            // int age = 22;
             // C# variables with concatenation example.
-            Console.WriteLine("Hello everyone! My name is "+name+" and my age is "+age);
+            // Console.WriteLine("Hello everyone! My name is "+name+" and my age is "+age);
 
-            Console.WriteLine("Enter Your Name: ");
-            name = Console.ReadLine();
-            Console.WriteLine("Enter your age: ");
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Hello everyone! My name is "+name+" and my age is "+age);
+            // Console.WriteLine("Enter Your Name: ");
+            // name = Console.ReadLine();
+            // Console.WriteLine("Enter your age: ");
+            // age = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Hello everyone! My name is "+name+" and my age is "+age);
+
+            // SECTION Day 3
+            // ---------------------------- Arithmetic Operators
+            // int x,y, addition, subtraction, multiply, division;
+            // Console.WriteLine("Enter first number: ");
+            // x = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter second number: ");
+            // y = Convert.ToInt32(Console.ReadLine());
+
+            // addition = x + y;
+            // subtraction = x - y;
+            // multiply = x * y;
+            // division = x/y;
+
+            // Console.WriteLine("The numbers you entered are: " + x + ", " + y);
+            // Console.WriteLine("After addition: " + addition);
+            // Console.WriteLine("After subtraction: " + subtraction);
+            // Console.WriteLine("After multiplication: " + multiply);
+            // Console.WriteLine("After division: " + division);
+
+            // ------------------------------ To check if given number is even or odd
+            // int num1;
+            // Console.WriteLine("Enter the number to check whether it's odd or even: ");
+            // num1 = Convert.ToInt32(Console.ReadLine());
+            // if (num1%2 == 0)
+            // Console.WriteLine("The given number " + num1 + " is even.");
+            // else
+            // Console.WriteLine("The given number "+ num1 + " is odd.");
+
+            // -------------------------------- To print * pattern
+            int nlines;
+            Console.WriteLine("Enter the number of lines you want to print: ");
+            nlines = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=1; i<=nlines; i++){
+                for(int j = i; j<=nlines/2; j++){
+                    Console.Write(" ");
+                }
+                for (int k = 1; k<=2*i-1; k=k+1){
+                Console.Write("*");
+                }
+                Console.Write("\n");
+            }
 
         }
     }
