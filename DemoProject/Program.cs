4f3e2c2683cb8e25c07a308f715eef6902d5fcc5
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-11-03 13:11:37 +0545

    demo project

---
+++ DemoProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DemoProject{
+    class TestOperator{
+        static void Main(string[] args){
+            // Console.WriteLine("hi");
+            string a = "hello";
+            string b = "sakshi";
+            Console.WriteLine(a+b);
+            // Console.WriteLine(a-b); # Error
+        }
+    }
+}
